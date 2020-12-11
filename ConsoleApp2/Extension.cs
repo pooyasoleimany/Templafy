eb53a96e7e65eb574340c353418ffd91f00ce6d6
Pooya Soleimany Nezhadian <6709962+pooyasoleimany@users.noreply.github.com>
2020-12-11 12:09:53 -0500

    Update Extension.cs

--- ConsoleApp2/Extension.cs
+++ ConsoleApp2/Extension.cs
@@ -1,86 +1,52 @@
-﻿using System;
+/*
+Name changes 
+    CheckThisNodeIsFirstChildOfParent => IsFirstChild
+    GetPreviousThisNode => GetPreviousSibling 
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
 namespace ConsoleApp2
 {
 
-    public static class NodeExtensions
-    {
+	public static class NodeExtensions
+	{
+		public static Node Previous(this Node node)
+		{
 
-        private static readonly Stack<Node> NodeStack = new Stack<Node>();
-        public static Node Previous(this Node node)
-        {
+			var parent = node.Parent;
+			if (parent == null) return null;
 
-            var parentNode = node.Parent;
-            if (CheckParentNodeIsNull(parentNode)) return null;
+			if (parent.IsFirstChild(node)) return parent;
 
-            if (CheckThisNodeIsFirstChildOfParent(node, parentNode)) return parentNode;
+			var leftSibling = node.GetLeftSibling();
+			var previous = leftSibling.GetLastInSubtree();
+			
+			return previous;
+		}
 
-            var previousSiblingNode = GetPreviousThisNode(node, parentNode);
-            return previousSiblingNode;
+		private static Node GetLeftSibling(this Node rightChild)
+		{
+			Node previousChild = null;
+			foreach (var child in rightChild.Parent.Children)
+			{
+				if (child == rightChild)
+					break;
+					
+				previousChild = child;
+			}
+			return previousChild;
+		}
 
-        }
+		private static Node GetLastInSubtree(this Node node)
+		{
+			while (node.Children.Any())
+				node = node.Children.Last();
+			return node;
+		}
 
-
-        public static void GetLastLeave(Node node)
-        {
-            if (node.Children.Count() == 0)
-            {
-                NodeStack.Push(node);
-            }
-            else
-            {
-                foreach (Node child in node.Children)
-                {
-                    NodeStack.Push(child);
-                    GetLastLeave(child); //<-- recursive
-                }
-            }
-
-        }
-
-        private static bool CheckParentNodeIsNull(Node parent)
-        {
-            if (parent == null)
-                return true;
-            else
-                return false;
-        }
-
-        private static bool CheckThisNodeIsFirstChildOfParent(Node node, Node parent)
-        {
-            var firstChildParentNode = parent.Children.First();
-            if (node == firstChildParentNode) // node is the first child
-                return true;
-            else
-                return false;
-
-        }
-
-
-        private static Node GetPreviousThisNode(Node node, Node parent)
-        {
-            for (int i = 0; i <= parent.Children.Count(); i++)
-            {
-            
-                if (parent.Children.ElementAt(i) == node)
-                {
-                    if(parent.Children.ElementAt(i - 1).Children.Count()==0)
-                    return parent.Children.ElementAt(i - 1);
-
-                    else
-                    {
-                        GetLastLeave(parent.Children.ElementAt(i - 1));
-                        return NodeStack.Pop();
-                    }
-
-                }
-
-            }
-            return null;
-
-        }
-    }
+		private static bool IsFirstChild(this Node parent, Node node) =>
+			parent.Children.FirstOrDefault() == node;
+	}
 }
